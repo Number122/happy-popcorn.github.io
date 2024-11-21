611d4687a112a23b3a35cf63d73f6f1bb69402cd
Number122 <73112439+Number122@users.noreply.github.com>
2024-11-21 20:04:52 +0330

    Add Cart

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const modal = document.getElementById("cartModal");
-
 const addToCartButtons = document.querySelectorAll(".product button");
-
 const closeButton = document.getElementsByClassName("close")[0];
+let cart = [];
 
 addToCartButtons.forEach(function(button) {
     button.addEventListener("click", function() {
@@ -35,3 +34,32 @@ document.getElementById("cartForm").addEventListener("submit", function(event) {
     alert("Thank you, " + userName + "! Your " + document.getElementById("flavorName").innerText + " has been added to your cart.");
     modal.style.display = "none";
 });
+
+function addToCart(name, price) {
+    cart.push({ name, price });
+    document.getElementById('cartCount').innerText = cart.length;
+    updateCartDisplay();
+    document.getElementById('flavorName').innerText = name;
+    document.getElementById('currentDate').innerText = new Date().toLocaleDateString();
+    document.getElementById('currentTime').innerText = new Date().toLocaleTimeString();
+    document.getElementById('cartModal').style.display = 'block';
+}
+
+function updateCartDisplay() {
+    const cartItemsDiv = document.getElementById('cartItems');
+    cartItemsDiv.innerHTML = '';
+    cart.forEach(item => {
+        const itemDiv = document.createElement('div');
+        itemDiv.innerText = `${item.name} - $${item.price.toFixed(2)}`;
+        cartItemsDiv.appendChild(itemDiv);
+    });
+}
+
+function checkout() {
+    alert('Proceeding to checkout...');
+    //TODO: Edit this
+}
+
+document.querySelector('.close').onclick = function() {
+    document.getElementById('cartModal').style.display = 'none';
+}
